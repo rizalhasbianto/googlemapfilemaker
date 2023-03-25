b80a6120a722b9728ca1141ebbde49176bbca3f0
rizalhasbianto <rizalhasbianto@gmail.com>
2023-03-25 23:42:45 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,480 @@
+// In the following example, markers appear when the user clicks on the map.
+// The markers are stored in an array.
+// The user can then click an option to hide, show or delete the markers.
+let map;
+let markers = [];
+let neighborhoodMarkers = [];
+let markersFilter = [];
+let url = 'https://BluePrintMap.hellomuto.repl.co/properties';
+let markerPosition = []
+let neighborhoodList = []
+
+const splitMarkerZoom = 13;
+
+function initMap() {
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: new google.maps.LatLng(47.60507343621495, -122.32353164431197),
+    mapTypeId: "terrain",
+  });
+}
+
+fetch(url, {
+  method: 'GET',
+  //credentials: 'user:passwd'
+})
+.then(response => response.text())
+.then(str => new DOMParser().parseFromString(str, "text/xml"))
+.then(data => {
+  // SHOWING PROPERTIES AND ADD MARKER TO MAP
+  initData(data);
+console.log(data)
+  // filter function
+  const filterSelect = document.querySelectorAll('.map-filter');
+  const propList = document.querySelectorAll('.prop-list-wrap');
+
+  for (let r = 0; r < filterSelect.length; r++) {
+    filterSelect[r].addEventListener('change', function handleChange() {
+      const neighborhoodSelected = document.querySelector('.filter-neighborhood select').value;
+      const typeSFR = document.getElementById("sfr").checked
+      const typeTH = document.getElementById("th").checked
+      const lowPriceSelected = document.querySelector('#one').value;
+      const highPriceSelected = document.querySelector('#two').value;
+
+      let typeSelected = []
+
+      if(typeSFR) {
+        typeSelected.push("SFR")
+      }
+      if(typeTH) {
+        typeSelected.push("TH")
+      }
+
+      // DATA FILTER
+      dataFilter(neighborhoodSelected, lowPriceSelected, highPriceSelected, propList, typeSelected);
+  
+      // GOOGLE MAPS
+      filterMarker(neighborhoodSelected, lowPriceSelected, highPriceSelected, typeSelected);
+      //centerMap(neighborhoodSelected);
+    });
+  }
+
+  google.maps.event.addListener(map, 'idle', function() {
+    // FILTER PROPERTY BASED ON MAP BOUND
+    for (let i = 0; i < propList.length; i++) {
+      const currentLatLang = JSON.parse(propList[i].getAttribute("latlang"));
+      const filterStatus = propList[i].getAttribute("filter");
+      const mapBound = map.getBounds().contains(currentLatLang);
+      if(mapBound && filterStatus == "include") {
+        propList[i].style.display = "block";
+      } else {
+        propList[i].style.display = "none";
+      }
+    }
+  });
+
+  google.maps.event.addListener(map, 'zoom_changed', function() {
+    // FILTER MARKER BASED ON ZOOM
+    const zoomLevel = map.getZoom();
+    if( zoomLevel >= splitMarkerZoom) {
+      markers.forEach((marker,i) => {
+        const filterStatus = propList[i].getAttribute("filter");
+        if(filterStatus == "include") {
+          marker.setVisible(true)
+        }
+      })
+      neighborhoodMarkers.forEach((marker) => {
+        marker.setVisible(false)
+      })
+    } else {
+      markers.forEach((marker) => {
+        marker.setVisible(false)
+      })
+      neighborhoodMarkers.forEach((marker) => {
+        marker.setVisible(true)
+      })
+    }
+  });
+
+  // Search function
+  const searchInput = document.querySelector('.map-search');
+  searchInput.oninput = function () {
+    searchAddress(searchInput.value)
+  }
+
+  // Sort Function
+  const sortSelect = document.querySelector('.map-sort');
+  sortSelect.addEventListener('change', function handleChange() {
+
+    var dataProperties = document.querySelectorAll('.prop-list-wrap')
+    dataProperties = Array.prototype.slice.call(dataProperties)
+
+    if(this.value == "pricehightolow") {
+      dataProperties.sort(function(a, b){
+        return b.children[1].children[2].textContent - a.children[1].children[2].textContent
+      });
+    }
+    if(this.value == "pricelowToHigh") {
+      dataProperties.sort(function(a, b){
+        return a.children[1].children[2].textContent - b.children[1].children[2].textContent
+      });
+    }
+    if(this.value == "type") {
+      dataProperties.sort(function(a, b){
+        let before
+        let after
+        if(a.children[1].children[1].textContent == "SFR"){
+          before = 1
+        } else {
+          before = 2
+        }
+        if(b.children[1].children[1].textContent == "SFR"){
+          after = 1
+        } else {
+          after = 2
+        }
+
+        return before - after
+      });
+    }
+    if(this.value == "status") {
+      dataProperties.sort(function(a, b){
+        let before
+        let after
+        if(a.children[1].children[3].textContent == "On Market"){
+          before = 1
+        } else if(a.children[1].children[3].textContent == "Under Construction") {
+          before = 2
+        } else {
+          before = 3
+        }
+        if(b.children[1].children[3].textContent == "On Market"){
+          after = 1
+        } else if(b.children[1].children[3].textContent == "Under Construction") {
+          after = 2
+        } else {
+          after = 3
+        }
+
+        return before - after
+      });
+    }
+    
+    for (let i = 0; i < dataProperties.length; i++) {
+      dataProperties[i].parentNode.appendChild(dataProperties[i]);
+    }
+  });
+
+  // Single property link function
+  const singleProLink = document.querySelectorAll('.single-property-link');
+
+  for (let i = 0; i < singleProLink.length; i++) {
+    singleProLink[i].addEventListener("click", function(event){
+      event.preventDefault();
+      const price = this.querySelector(".price").textContent;
+      sessionStorage.setItem("price", price);
+      window.open('./single-property.html', '_blank');
+    });
+  }
+});
+
+// SHOWING PROPERTIES AND ADD MARKER TO MAP
+function initData(data) {
+  const dataProperties = data.getElementsByTagName("record");
+  const target = document.querySelector('.properties');
+  const targetFilterCity = document.querySelector('.filter-neighborhood');
+
+  var select = document.createElement("select"),
+      firstOption = document.createElement("option");
+  select.name = "neighborhood";
+  select.id = "neighborhood";
+  select.className = "map-filter"
+  firstOption.text = "All neighborhood";
+  select.appendChild(firstOption);
+  let infoWindow = new google.maps.InfoWindow();
+
+  for (let i = 0; i < dataProperties.length; i++) {
+      const id = dataProperties[i]?.children[7]?.textContent;
+      const recId = dataProperties[i]?.attributes[1]?.nodeValue;
+      const address = dataProperties[i]?.children[0]?.textContent;
+      const city = dataProperties[i]?.children[3]?.textContent;
+      const state = dataProperties[i]?.children[4]?.textContent;
+      const postalCode = dataProperties[i]?.children[5]?.textContent;
+      const neighborhood = dataProperties[i]?.children[13]?.textContent;
+      const bathrooms = dataProperties[i]?.children[10]?.textContent;
+      const bedrooms = dataProperties[i]?.children[11]?.textContent;
+      const type = dataProperties[i]?.children[15]?.textContent;
+      const status = dataProperties[i]?.children[18]?.textContent;
+      const price = Number(dataProperties[i]?.children[17]?.textContent);
+      const img = dataProperties[i]?.children[27]?.textContent;
+      const locationData = dataProperties[i]?.children[2]?.textContent.split(",");
+
+      const markerData = {
+        position: { lat: Number(locationData[0]), lng: Number(locationData[1]) }, 
+        neighborhood,
+        address,
+        state,
+        postalCode,
+        bathrooms: bathrooms + " bath",
+        bedrooms: bedrooms + "bed",
+        type,
+        status,
+        price,
+        img,
+        recId,
+        id
+      };
+
+      addMarker(markerData, infoWindow);
+
+      var wraper = document.createElement("div"),
+          option = document.createElement("option");
+      wraper.className = "prop-list-wrap";
+      wraper.setAttribute("latLang",`{"lat": ${Number(locationData[0])}, "lng": ${Number(locationData[1])}}`);
+      wraper.setAttribute("filter", "include");
+      wraper.setAttribute("filter-map", "include");
+
+      if (address) {
+        if (city) {
+          wraper.setAttribute("neighborhood", neighborhood);
+          const availableCity = neighborhoodList.find(list => list.name == neighborhood);
+            if (!availableCity) {
+                option.value = neighborhood;
+                option.text = neighborhood;
+                select.appendChild(option);
+                neighborhoodList.push({
+                  name: neighborhood,
+                  total: 1
+                });
+            } else {
+              availableCity.total = availableCity.total + 1
+            }
+        }
+        if (price) {
+          wraper.setAttribute("price", price);
+        }
+        if (type) {
+          wraper.setAttribute("type", type);
+        }
+
+        const imgListInfo = markerData.img ? 'data:image/png;base64, ' + markerData.img : './no-image.png'
+        const propertyType = markerData.type == "TH" ? "Townhouse" : "Single Family Residence"
+        const formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        });
+
+        wraper.innerHTML = `
+        <a href="./single-property.html?recId=${markerData.recId}" class="single-property-link">
+          <div class="img-wrap">
+            <img src="${imgListInfo}">
+          </div>
+          <h2>${markerData.status}</h2>
+          <div class="property-info">
+            <p class="price">${formatter.format(markerData.price)}</p>
+            <p class="property-address">${markerData.address}</p>
+            <p>${markerData.bedrooms} | ${markerData.bathrooms} ${propertyType}</p>
+          </div>
+        </a>
+        `;
+        target.appendChild(wraper);
+      }
+  }
+  // Get price range
+  const lowerInput = document.querySelector("#lower");
+  const upperInput = document.querySelector("#upper");
+  const InputOne = document.querySelector("#one");
+  const InputTwo = document.querySelector("#two");
+  const minPrice = Math.min(...markers.map(item => item.price));
+  const maxPrice = Math.max(...markers.map(item => item.price));
+  lowerInput.setAttribute("min",minPrice);
+  lowerInput.setAttribute("max",maxPrice);
+  lowerInput.value = minPrice;
+  InputOne.value = minPrice;
+  upperInput.setAttribute("min",minPrice);
+  upperInput.setAttribute("max",maxPrice);
+  upperInput.value = maxPrice;
+  InputTwo.value = maxPrice;
+
+  // add Neighborhood filter based on data
+  targetFilterCity.appendChild(select);
+
+  // Add Neighborhood marker
+  addNeighborhoodMarker(neighborhoodList)
+  
+  // ZOOM Fit to markers
+  let latlngbounds = new google.maps.LatLngBounds();
+  for (let i = 0; i < markers.length; i++) {
+    latlngbounds.extend(markers[i].position);
+  }
+  map.fitBounds(latlngbounds);
+  var zoom = map.getZoom();
+  map.setZoom(zoom > 12 ? 12 : zoom);
+}
+
+const neighborhoodFilter = (getNeighborhoodAttr, neighborhoodSelected) => {
+  if(neighborhoodSelected == "All neighborhood") {
+    return true;
+  } 
+  if (getNeighborhoodAttr == neighborhoodSelected) {
+    return true;
+  } 
+  return false;
+}
+
+const priceFilter = (getPriceAttr, lowPriceSelect, highPriceSelect) => {
+  if(getPriceAttr >= Number(lowPriceSelect) && getPriceAttr <= Number(highPriceSelect)) {
+    return true;
+  }
+  return false;
+}
+
+const typeFilter = (getTypeAttr, typeSelected) => {
+  if(typeSelected.includes(getTypeAttr)) {
+    return true;
+  }
+  return false;
+}
+
+function dataFilter(neighborhoodSelected, lowPriceSelected, highPriceSelected, propList, typeSelected) {
+    if (propList) {
+        for (let i = 0; i < propList.length; i++) {
+            const getNeighborhoodAttr = propList[i].getAttribute("neighborhood");
+            const getPriceAttr = propList[i].getAttribute("price");
+            const getTypeAttr = propList[i].getAttribute("type");
+            const currentLatLang = JSON.parse(propList[i].getAttribute("latlang"));
+            const mapBound = map.getBounds().contains(currentLatLang);
+            const zoomLevel = map.getZoom();
+              if (
+                neighborhoodFilter(getNeighborhoodAttr, neighborhoodSelected) && 
+                priceFilter(getPriceAttr, lowPriceSelected, highPriceSelected) &&
+                typeFilter(getTypeAttr, typeSelected)
+                ) {
+                  if( mapBound ) {
+                    propList[i].style.display = "block";
+                  }
+                  propList[i].setAttribute("filter","include")
+              } else {
+                  propList[i].style.display = "none";
+                  propList[i].setAttribute("filter","exclude")
+              }
+        }
+    }
+}
+
+// MAP FUNCTION
+// add neighborhood marker
+
+async function addNeighborhoodMarker( neighborhoodList ) {
+  for (let i = 0; i < neighborhoodList.length; i++) {
+    const findNeighborhoodGeo = await getAddressLatLang(neighborhoodList[i].name)
+    const neighborhoodMarker = new google.maps.Marker({
+      position: findNeighborhoodGeo.location,
+      map,
+      icon: {
+        url:"./img/marker-blue.png",
+        scaledSize: new google.maps.Size(30, 30)
+      },
+      visible: true,
+      optimized: false,
+      label:`${neighborhoodList[i].total}`,
+    });
+    neighborhoodMarkers.push(neighborhoodMarker)
+  }
+}
+
+// find address based on input
+async function searchAddress( address ) {
+  const findAddressGeo = await getAddressLatLang(address)
+  map.fitBounds(findAddressGeo.bounds);
+}
+
+// find bound of area
+const getAddressLatLang = address => {
+  return new Promise((resolve, reject) => {
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({address: address + "Seattle USA"}, (results, status) => {
+          if (status === 'OK') {
+              resolve(results[0].geometry);
+          } else {
+              reject(status);
+          }    
+      });    
+  });
+};
+
+// add single marker
+function addMarker( markerData, infoWindow ) {
+  const marker = new google.maps.Marker({
+    position: markerData.position,
+    map,
+    neighborhood: markerData.neighborhood,
+    price: markerData.price,
+    type: markerData.type,
+    visible: false,
+    icon: {
+      url:"./img/property-marker.png"
+    },
+  });
+
+  const imgListInfo = markerData.img ? 'data:image/png;base64, ' + markerData.img : './no-image.png'
+  const contentString =
+    '<div id="content" class="marker-info">' +
+    `<h2 class="firstHeading">${markerData.address}, ${markerData.state} ${markerData.postalCode}</h1>` +
+    '<div id="bodyContent">' +
+    '<div class="detail-info">' +
+    "<p> " +
+    `<span>${markerData.bedrooms}, ${markerData.bathrooms}</span><br/>` +
+    `<span>${markerData.address}</span><br/>` +
+    `<span>${markerData.type}</span><br/>` +
+    `<span>${markerData.status}</span><br/>` +
+    "</p>" +
+    `<img src='${imgListInfo}' />` +
+    "</div>" +
+    '<p><a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "Details...</a></p>" +
+    "</div>" +
+    "</div>";
+
+  marker.addListener("click", () => {
+    infoWindow.setContent(contentString)
+    infoWindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+
+  markers.push(marker);
+}
+
+// Filter markers at map
+function filterMarker(neighborhoodSelected, lowPriceSelected, highPriceSelected, typeSelected) {
+  //let latlngbounds = new google.maps.LatLngBounds();
+  for (let i = 0; i < markers.length; i++) {
+    const getNeighborhoodAttr = markers[i].neighborhood;
+    const getPriceAttr = markers[i].price;
+    const getTypeAttr = markers[i].type;
+    const zoomLevel = map.getZoom();
+    if (
+      neighborhoodFilter(getNeighborhoodAttr, neighborhoodSelected) && 
+      priceFilter(getPriceAttr, lowPriceSelected, highPriceSelected) &&
+      typeFilter(getTypeAttr, typeSelected)
+      ) {
+        if(zoomLevel >= splitMarkerZoom)
+        markers[i].setVisible(true);
+      //latlngbounds.extend(markers[i].position);
+    } else {
+      markers[i].setVisible(false);
+    }
+  }
+  //map.fitBounds(latlngbounds);
+  //var zoom = map.getZoom();
+  //map.setZoom(zoom > 12 ? 12 : zoom);
+}
+
+function check_is_in_or_out(marker){
+  return map.getBounds().contains(marker.getPosition());
+}
+
+window.initMap = initMap;
---
+++ js/single-property.js
@@ -0,0 +1,90 @@
+// Getting Property Detail
+const urlPropertyDetail = 'https://BluePrintMap.hellomuto.repl.co/property-detail',
+      urlPropertyVendor = 'https://BluePrintMap.hellomuto.repl.co/loan-detail',
+      urlPropertyImg = 'https://BluePrintMap.hellomuto.repl.co/img-detail',
+      queryString = window.location.search,
+      urlParams = new URLSearchParams(queryString),
+      recId = `?recId=${urlParams.get('recId')}`
+
+fetch(urlPropertyDetail+recId, {
+  method: 'GET'
+  //credentials: 'user:passwd'
+})
+.then(response => response.text())
+.then(str => new DOMParser().parseFromString(str, "text/xml"))
+.then(data => {
+  //console.log(data)
+  const dataProperties = data.getElementsByTagName("record");
+  initData(dataProperties[0])
+  loadLoanData(dataProperties[0])
+  loadImgdata(dataProperties[0])
+})
+
+function loadLoanData(dataProperties) {
+  const field = dataProperties?.children[6]?.textContent,
+        loanId = `?loanId=${field}`
+  fetch(urlPropertyVendor+loanId, {
+    method: 'GET'
+    //credentials: 'user:passwd'
+  })
+  .then(response => response.text())
+  .then(str => new DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const dataVendor = data.getElementsByTagName("record");
+    const vendorTarget = document.querySelector(".vendor")
+    for (let i = 0; i < dataVendor.length; i++) {
+      console.log(dataVendor[i])
+      var wraper = document.createElement("div")  
+      wraper.className = "vendor-list"
+      wraper.innerHTML = `
+        <h2 class="vendor-role">${dataVendor[i].children[0]?.textContent}</h2>
+        <h3 class="vendor-name">${dataVendor[i].children[6]?.textContent}</h3>
+        <p class="vendor-detail">${dataVendor[i].children[3]?.textContent}</p>
+        <p class="vendor-phone">${dataVendor[i].children[9]?.textContent}</p>
+        <p class="vendor-email">${dataVendor[i].children[4]?.textContent}</p>
+        `;
+      vendorTarget.appendChild(wraper)
+    }
+  })
+}
+
+function loadImgdata(dataProperties) {
+  const field = dataProperties?.children[7]?.textContent,
+        id = `?id=${field}`
+  fetch(urlPropertyImg+id, {
+    method: 'GET'
+    //credentials: 'user:passwd'
+  })
+  .then(response => response.text())
+  .then(str => new DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    console.log(data)
+    const dataVendor = data.getElementsByTagName("record");
+    const imgElement = document.querySelector(".main-img img")
+    const imgData = dataVendor[0]?.children[11]?.textContent
+    imgElement.setAttribute("src", `data:image/png;base64, ${imgData}`)
+  })
+}
+
+//Print data to html
+function initData(data) {
+  const propertyData = data?.children;
+
+  for (let i = 0; i < propertyData.length; i++) {
+    const elementName = propertyData[i].getAttribute("name");
+    const className = elementName.replace("::","")
+    const dataValue = propertyData[i]?.textContent
+    const elementTarget = document.querySelector(`.${className}`)
+    if(elementTarget) {
+      if( className == "z_WebPrice") {
+        const formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        });
+        elementTarget.textContent = formatter.format(dataValue)
+      } else {
+          elementTarget.textContent = dataValue
+      }
+    }
+  }
+}
