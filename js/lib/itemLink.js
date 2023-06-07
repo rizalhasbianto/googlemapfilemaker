208e9e87b10810a69dc8dfca5cdca1b944714a5c
Jballeras <rizalhasbianto@gmail.com>
2023-06-07 11:17:35 +0700

    test

--- js/lib/itemLink.js
+++ js/lib/itemLink.js
@@ -1,9 +1,10 @@
 function itemLink(singleProLink, data) {
     for (let i = 0; i < singleProLink.length; i++) {
-        singleProLink[i].addEventListener("click", function(event){
+        singleProLink[i].addEventListener("click", function(event){ 
             event.preventDefault();
             sessionStorage.setItem(`singleProperty_${i}`, JSON.stringify(data[i]));
-            window.open(`./single-property?recId=${i}`);
+            console.log("index ",i)
+            //window.open(`./single-property?recId=${i}`);
         });
     }
 }
