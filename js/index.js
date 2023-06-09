f68c71c79b51777d836e3e127fcd4c51537618fe
rizalhasbianto <rizalhasbianto@gmail.com>
2023-06-09 13:43:16 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,15 +29,3 @@ fetch(url, {
   // SHOWING PROPERTIES AND ADD MARKER TO MAP
   initData(data, map);
 });
-
-// Load big img to properties
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-  );
-}
