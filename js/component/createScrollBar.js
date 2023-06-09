e03297b94098297df2c918c6c440d74a4661db86
rizalhasbianto <rizalhasbianto@gmail.com>
2023-06-09 13:42:52 +0700

    Update createScrollBar.js

--- js/component/createScrollBar.js
+++ js/component/createScrollBar.js
@@ -1,4 +1,17 @@
-function createScrollBar(target) {
+import { getBigImgdata } from "../lib/getBigImgdata.js"
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  
+    );
+  }
+
+function createScrollBar(target,staticImgUrl) {
     const scrollbarWraper = document.querySelector(".scroll-fake");
     const propertiesWraper = document.querySelector(".info");
     const filterElement = propertiesWraper.querySelector(".filter--wrapper");
@@ -18,9 +31,20 @@ function createScrollBar(target) {
         propertiesWraper.scrollTop = this.value
         },
     })
+
     scrollbarWraper.appendChild(scrollbar)
     propertiesWraper.addEventListener("scroll", function() {
         scrollbar.value = this.scrollTop;
+
+        for(i=0; i < propertiesWraper.length; i++ ) {
+            const propOnView = isInViewport(propertiesWraper[i]);
+            const imgLoaded = propertiesWraper[i].getAttribute("img")
+            if(!imgLoaded && propOnView) {
+              const id = propertiesWraper[i].getAttribute("id")
+              getBigImgdata(id, staticImgUrl)
+              propertiesWraper[i].setAttribute("img",true);
+            }
+        }
     });
 }
 
